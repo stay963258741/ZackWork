a7f7ac88df9b79948f89ea4eadc41016e6f1fd01
zack <zack@chenyurende-macbook-pro.local>
2020-12-01 14:29:13 +0800

    Set delete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.names', '新增網址') }}</title>
+    <title>{{ config('app.names', '網址管理') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
