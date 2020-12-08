aa8150707582b086ff8acaf9e2f85182aa10a726
zack <zack@chenyurende-macbook-pro.local>
2020-12-08 17:03:18 +0800

    first commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,15 +35,18 @@ class Kernel extends ConsoleKernel
                     // $status = $client->get($address->hostname);   guzzle
                     $status = Http::get($address->hostname);
                     if ($status->status() == 200){
-                        $address->update(['status' => 1 ]);
+                        Address::where('hostname','=',$address->hostname)
+                            ->update(['status'=>1]);
                     }else{
                         if ($address->status == true) {
                             @BotJob::dispatch();
                         }
-                        $address->update(['status' => 0 ]);
+                        Address::where('hostname','=',$address->hostname)
+                            ->update(['status'=>0]);
                     }
                 }catch (\Exception $ex){
-                    $address->update(['status' => 0 ]);
+                    Address::where('hostname','=',$address->hostname)
+                        ->update(['status'=>0]);
                 }
             }
         })->everyMinute();
--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -34,17 +34,20 @@ class AddressController extends Controller
                 // $status = $client->get($address->hostname);   guzzle
                 $status = Http::get($address->hostname);
                 if ($status->status() == 200){
-                    $address->update(['status' => 1 ]);
+                    Address::where('hostname','=',$address->hostname)
+                        ->update(['status'=>1]);
                 }else{
                     if ($address->status == true) {
                         $add =  $address->hostname ;
                         $link = 'https://api.telegram.org/bot1422625730:AAEoBRxtV1xfZYjyrI8uL0bf0KKN4xK706w/sendMessage?chat_id=-476202703&text=網址錯誤或網站已關閉·請檢查:';
                         Http::get($link.$add);
                     }
-                    $address->update(['status' => 0 ]);
+                    Address::where('hostname','=',$address->hostname)
+                        ->update(['status'=>0]);
                 }
             }catch (\Exception $ex){
-                $address->update(['status' => 0 ]);
+                Address::where('hostname','=',$address->hostname)
+                    ->update(['status'=>0]);
             }
         }
     }
