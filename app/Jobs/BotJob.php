6fc45f99280119dff3508011c78c9fb1d7b6b7a0
zack <zack@chenyurende-macbook-pro.local>
2020-12-16 09:52:26 +0800

    first commit

--- app/Jobs/BotJob.php
+++ app/Jobs/BotJob.php
@@ -2,9 +2,7 @@
 
 namespace App\Jobs;
 
-use App\Models\Address;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
